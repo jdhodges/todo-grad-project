e95a79ed216e810b2c5005bff90ec13a8f4b9d81
jdhodges431 <jdhodges431@gmail.com>
2015-09-21 10:43:53 +0100

    Minimised function calls

--- public/main.js
+++ public/main.js
@@ -12,39 +12,61 @@ app.controller("TodoListController", ["$http", function($http) {
     todoListCtrl.newTodo = {};
     todoListCtrl.currentHover = -1;
     todoListCtrl.selectedFilter = -1;
-    todoListCtrl.activeTasks = 0;
-    todoListCtrl.completedTasks = 0;
+    todoListCtrl.activeTasks = -1;
+    todoListCtrl.completedTasks = -1;
 
     this.setClear = function(id) {
+        console.log("Set Clear");
         todoListCtrl.currentHover = id;
     };
 
     this.clearCheck = function(id) {
+        console.log("Clear check");
         return todoListCtrl.currentHover === id;
     };
 
     this.refreshTodoList = function() {
+        console.log("Refresh");
         $http.get("/api/todo").success(function (data) {
             todoListCtrl.todos = data;
 
+            if (todoListCtrl.activeTasks === -1 && todoListCtrl.completedTasks === -1)
+                todoListCtrl.activeCount();
         });
     };
 
     this.addTodo = function(todo) {
+        console.log("Add");
         $http.post("api/todo", todo).success(function(data) {
+            todoListCtrl.activeTasks++;
             todoListCtrl.refreshTodoList();
             todoListCtrl.newTodo = {};
         });
     };
 
     this.completeTodo = function(todo) {
+        console.log("Complete");
         $http.put("api/todo/" + todo.id, {isComplete: !todo.isComplete}).success(function(data) {
+            if (todo.isComplete) {
+                todoListCtrl.activeTasks--;
+                todoListCtrl.completedTasks++;
+            } else {
+                todoListCtrl.activeTasks++;
+                todoListCtrl.completedTasks--;
+            }
             //todoListCtrl.refreshTodoList();
         });
     };
 
     this.deleteTodo = function(todo) {
+        console.log("Delete");
         $http.delete("api/todo/" + todo.id).success(function(data) {
+            if (todo.isComplete) {
+                todoListCtrl.completedTasks--;
+            } else {
+                todoListCtrl.activeTasks--;
+            }
+
             todoListCtrl.todos = todoListCtrl.todos.filter(function(otherTodo) {
                 return otherTodo !== todo;
             });
@@ -52,9 +74,11 @@ app.controller("TodoListController", ["$http", function($http) {
     };
 
     this.clearCompleted = function() {
+        console.log("Clear completed");
         [].forEach.call(todoListCtrl.todos, function(todo) {
             if (todo.isComplete) {
                 $http.delete("api/todo/" + todo.id).success(function(data) {
+                    todoListCtrl.completedTasks--;
                     todoListCtrl.todos = todoListCtrl.todos.filter(function(otherTodo) {
                         return otherTodo !== todo;
                     });
@@ -64,6 +88,7 @@ app.controller("TodoListController", ["$http", function($http) {
     };
 
     this.activeCount = function() {
+        console.log("Active count");
         todoListCtrl.activeTasks = 0;
         todoListCtrl.completedTasks = 0;
 
@@ -73,15 +98,10 @@ app.controller("TodoListController", ["$http", function($http) {
             else
                 todoListCtrl.activeTasks++;
         });
-
-        if (todoListCtrl.activeTasks === 1) {
-            return todoListCtrl.activeTasks + " task to complete";
-        }
-
-        return todoListCtrl.activeTasks + " tasks to complete";
     };
 
     this.applyFilter = function(todo) {
+        console.log("Apply filter");
         return (todoListCtrl.selectedFilter===1 && todo.isComplete) || (todoListCtrl.selectedFilter===2 && !todo.isComplete);
     };
 
